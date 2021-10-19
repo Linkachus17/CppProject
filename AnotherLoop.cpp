b7a7048153174fabd7df1facf564f97cdba46ecf
Linkachus17 <rg16111982@gmail.com>
2021-10-19 09:20:13 +0800

    Add files via upload

---
+++ AnotherLoop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    a = 0;
+    b = 1;
+    cout << "Counter Loop" << endl;
+    while (true){
+        cout << a++;
+        //Pause code should be here!
+    }
+}
---
+++ Loop.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    while (true){
+        cout << "This will loop forever until you terminate it!";
+    }
+}
