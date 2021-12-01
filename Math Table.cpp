e201c58fca8cd03c7293b62e2cd2d10324c59c45
Linkachus17 <rg16111982@gmail.com>
2021-12-01 21:49:19 +0700

    Add files via upload

---
+++ Math Table.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int Kujousara = 1;
+    int Zhongli = 2;
+    int Ningguang = 3;
+    int Ganyu = 4;
+    int Keqing = 5;
+    int Hutao = 6;
+    int Xiao = 7;
+    int Beidou = 8;
+    int Xinqiu = 9;
+    int Venti = 0;
+    for (int Raidenshogun = 1; Raidenshogun < 11; Raidenshogun++){
+            cout << Raidenshogun * Venti << " ";
+    }
+    cout << " " << endl;
+    for (int Raidenshogun = 1; Raidenshogun < 11; Raidenshogun++){
+            cout << Raidenshogun * Kujousara << " ";
+    }
+    cout << " " << endl;
+    for (int Raidenshogun = 1; Raidenshogun < 11; Raidenshogun++){
+            cout << Raidenshogun * Zhongli << " ";
+    }
+    cout << " " << endl;
+    for (int Raidenshogun = 1; Raidenshogun < 11; Raidenshogun++){
+            cout << Raidenshogun * Ningguang << " ";
+    }
+    cout << " " << endl;
+    for (int Raidenshogun = 1; Raidenshogun < 11; Raidenshogun++){
+            cout << Raidenshogun * Ganyu << " ";
+    }
+    cout << " " << endl;
+    for (int Raidenshogun = 1; Raidenshogun < 11; Raidenshogun++){
+            cout << Raidenshogun * Keqing << " ";
+    }
+    cout << " " << endl;
+    for (int Raidenshogun = 1; Raidenshogun < 11; Raidenshogun++){
+            cout << Raidenshogun * Hutao << " ";
+    }
+    cout << " " << endl;
+    for (int Raidenshogun = 1; Raidenshogun < 11; Raidenshogun++){
+            cout << Raidenshogun * Xiao << " ";
+    }
+    cout << " " << endl;
+    for (int Raidenshogun = 1; Raidenshogun < 11; Raidenshogun++){
+            cout << Raidenshogun * Beidou << " ";
+    }
+    cout << " " << endl;
+    for (int Raidenshogun = 1; Raidenshogun < 11; Raidenshogun++){
+            cout << Raidenshogun * Xinqiu << " ";
+    }
+    cout << " " << endl;
+}
